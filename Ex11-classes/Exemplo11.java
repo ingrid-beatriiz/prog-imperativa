983c6136927c35515c49dba3de8823cc61f82a9e
Ingrid Beatriz <108553706+ingrid-beatriiz@users.noreply.github.com>
2025-05-14 00:34:35 +0000

    Praticando com Classes

---
+++ Ex11-classes/Exemplo11.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Exemplo11 {
+    public static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa();
+    }
+}
+
+    

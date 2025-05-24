0a38ca6d33c776dc6025e3a514b293c1b7d4fb55
Ingrid Beatriz <108553706+ingrid-beatriiz@users.noreply.github.com>
2025-05-24 00:35:00 +0000

    Praticando com classes

--- Atividades/Ex01.java
+++ Atividades/Ex01.java
@@ -1,3 +1,13 @@
+// Vetores
+// 1. Faca um programa que possua um vetor denominado A que armazene 6 numeros intei- ´
+// ros. O programa deve executar os seguintes passos:
+// (a) Atribua os seguintes valores a esse vetor: 1, 0, 5, -2, -5, 7.
+// (b) Armazene em uma variavel inteira (simples) a soma entre os valores das posic¸ ´ oes ˜
+// A[0], A[1] e A[5] do vetor e mostre na tela esta soma.
+// (c) Modifique o vetor na posic¸ao 4, atribuindo a esta posic¸ ˜ ao o valor 100. ˜
+// (d) Mostre na tela cada valor do vetor A, um em cada linha.
+
+
 import java.util.Random;
 
 public class Ex01 {
@@ -5,11 +15,19 @@ public class Ex01 {
 
     public static void main(String[] args) {
 
-        int [] vetor = new int[5];
-        gerarNum(vetor);
+        int [] A = {1, 0, 5, -2, -5, 7};
+        soma(A);
+        // gerarNum(vetor);
         System.out.println();
-        imprimir(vetor);
+        imprimir(A);
         System.out.println();
+    }
+
+    public static void soma(int[] A) {
+
+        int soma = 0;
+        soma = A[0] + A[1] + A[5];
+        System.out.println(soma);
     }
 
     public static void gerarNum( int [] v) {
